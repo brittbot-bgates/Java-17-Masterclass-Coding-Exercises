d82aed8faf654d9a06b82b17b4ceb08b2130bfdf
Brittany Gates <support@bcgates.com>
2024-04-16 12:53:18 -0400

    Equal Sum Checker
    My solution to the coding exercise.

---
+++ EqualSumChecker.java
@@ -0,0 +1,22 @@
+public class EqualSumChecker {
+
+    /*
+    CODING EXERCISE INSTRUCTIONS
+    Write a method hasEqualSum with 3 parameters of type int.
+    The method should return boolean and it needs to return true if the sum of the first and second parameter is equal to the third parameter. Otherwise, return false.
+    */
+
+    public static boolean hasEqualSum(int num1, int  num2, int num3) {
+        if ((num1 + num2) == num3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+    }
+}
