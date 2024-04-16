62614e45145facaa6212fd98e3a9870a9471cfd0
Brittany Gates <support@bcgates.com>
2024-04-16 13:05:32 -0400

    Area Calculator
    My solution to the coding exercise.

---
+++ AreaCalculator.java
@@ -0,0 +1,35 @@
+public class AreaCalculator {
+
+    /*
+    CODING EXERCISE INSTRUCTIONS
+    Write a method named area with one double parameter named radius.
+    The method needs to return a double value that represents the area of a circle.
+    If the parameter radius is negative then return -1.0 to represent an invalid value.
+    Write another overloaded method with 2 parameters x and y (both doubles), where x and y represent the sides of a rectangle.
+    The method needs to return an area of a rectangle.
+    If either or both parameters is/are a negative return -1.0 to indicate an invalid value.
+    */
+
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1.0;
+        } else {
+            return Math.PI * (radius * radius);
+        }
+    }
+
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1.0;
+        } else {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+    }
+}
