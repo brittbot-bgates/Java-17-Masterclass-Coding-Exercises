5f128f84d74a1583cacd5c87168900299af66e22
Brittany Gates <support@bcgates.com>
2024-04-19 10:32:16 -0400

    Number Of Days In Month
    My solution to the coding exercise.

---
+++ NumberOfDaysInMonth.java
@@ -0,0 +1,72 @@
+public class NumberOfDaysInMonth {
+
+    /*
+    CODING EXERCISE INSTRUCTIONS
+    Write a method isLeapYear with a parameter of type int named year.
+    The parameter needs to be greater than or equal to 1 and less than or equal to 9999.
+    If the parameter is not in that range return false.
+    Otherwise, if it is in the valid range, calculate if the year is a leap year and return true if it is, otherwise return false.
+    A year is a leap year if it is divisible by 4 but not by 100, or it is divisible by 400.
+
+    Write another method getDaysInMonth with two parameters month and year. Both of type int.
+    If parameter month is < 1 or > 12 return -1.
+    If parameter year is < 1 or > 9999 then return -1.
+    This method needs to return the number of days in the month. Be careful about leap years they have 29 days in month 2 (February).
+    You should check if the year is a leap year using the method isLeapYear described above.
+    */
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12) {
+            return -1;
+        } else if (year < 1 || year > 9999) {
+            return -1;
+        } else {
+            switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12 -> {
+                    return 31;
+                }
+                case 2 -> {
+                    if (isLeapYear(year)) {
+                        return 29;
+                    } else {
+                        return 28;
+                    }
+                }
+                case 4, 6, 9, 11 -> {
+                    return 30;
+                }
+                default -> {
+                    return -1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDaysInMonth(1, 2020));
+        System.out.println(getDaysInMonth(2, 2020));
+        System.out.println(getDaysInMonth(2, 2018));
+        System.out.println(getDaysInMonth(-1, 2020));
+        System.out.println(getDaysInMonth(-11, -2020));
+    }
+}
